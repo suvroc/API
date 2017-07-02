d3144d684c770c16c9b32c17bd1b2f5e7c92db5b
A763847 <alon.amsalem@gmail.com>
2017-07-02 21:48:54 +0300

    build notification messages

--- src/AnyStatus.API/Items/Build.cs
+++ src/AnyStatus.API/Items/Build.cs
@@ -5,19 +5,19 @@
         public override Notification CreateNotification()
         {
             if (State == State.Ok)
-                return new Notification($"Build {Name} completed", NotificationIcon.Info);
+                return new Notification($"{Name} completed successfully", NotificationIcon.Info);
 
             if (State == State.PartiallySucceeded)
-                return new Notification($"Build {Name} partially succeeded", NotificationIcon.Warning);
+                return new Notification($"{Name} partially succeeded", NotificationIcon.Warning);
 
             if (State == State.Running)
-                return new Notification($"Build {Name} started", NotificationIcon.Info);
+                return new Notification($"{Name} started", NotificationIcon.Info);
 
             if (State == State.Queued)
-                return new Notification($"Build {Name} is queued", NotificationIcon.Info);
+                return new Notification($"{Name} has been queued", NotificationIcon.Info);
 
             if (State == State.Canceled)
-                return new Notification($"Build {Name} has been cancelled", NotificationIcon.Info);
+                return new Notification($"{Name} has been cancelled", NotificationIcon.Info);
 
             return base.CreateNotification();
         }
