7bdb63d0720266555608b90bb359688c546f87f6
A763847 <alon.amsalem@gmail.com>
2017-08-12 17:07:08 +0300

    moved file dialogs to IDialogService

--- src/AnyStatus.API/Utils/IDialogService.cs
+++ src/AnyStatus.API/Utils/IDialogService.cs
@@ -7,5 +7,9 @@ namespace AnyStatus.API
         MessageBoxResult Show(string text, string title, MessageBoxButton button, MessageBoxImage image);
 
         void ShowWarning(string v1, string v2);
+
+        bool? ShowSaveFileDialog(string filter, out string fileName);
+
+        bool? ShowOpenFileDialog(string filter, out string fileName);
     }
 }
