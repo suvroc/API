9903b6bbbe212e84319ef7e903543685b04ffc42
A763847 <alon.amsalem@gmail.com>
2017-08-25 18:37:11 +0300

    hide root item

--- src/AnyStatus.API/Plugins/RootItem.cs
+++ src/AnyStatus.API/Plugins/RootItem.cs
@@ -7,6 +7,7 @@ using System.Xml.Serialization;
 
 namespace AnyStatus.API
 {
+    [Browsable(false)]
     public class RootItem : Folder, IXmlSerializable
     {
         const string StaticName = "RootItem";
