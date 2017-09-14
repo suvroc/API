1e657386d5df4719d815f1301ddc697a3b332ac8
A763847 <alon.amsalem@gmail.com>
2017-09-14 20:19:50 +0300

    moved aggregate  behavior to base item

--- src/AnyStatus.API/Plugins/Folder.cs
+++ src/AnyStatus.API/Plugins/Folder.cs
@@ -1,130 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Linq;
-using System.Xml.Serialization;
+﻿using System.ComponentModel;
 
 namespace AnyStatus.API
 {
     public class Folder : Item
     {
-        private int _count;
-        private readonly bool _aggregateState;
+        public Folder() : base(aggregateState: true) { }
 
-        public Folder() : this(aggregateState: true)
-        {
-        }
-
-        public Folder(bool aggregateState)
-        {
-            _aggregateState = aggregateState;
-
-            Items = new ObservableCollection<Item>();
-
-            Items.CollectionChanged += OnCollectionChanged;
-        }
-
-        /// <summary>
-        /// The number of child items with same state
-        /// </summary>
-        [XmlIgnore]
-        [Browsable(false)]
-        public int Count
-        {
-            get { return _count; }
-            set
-            {
-                _count = value;
-                OnPropertyChanged();
-            }
-        }
+        protected Folder(bool aggregateState) : base(aggregateState: true) { }
 
         [Browsable(false)]
         public new int Interval { get; set; }
 
         [Browsable(false)]
         public new bool ShowNotifications { get; set; }
-
-        public void Clear()
-        {
-            Items?.Clear();
-        }
-
-        public bool Contains(Item item)
-        {
-            return Items != null && Items.Contains(item);
-        }
-
-        private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
-        {
-            if (!_aggregateState) return;
-
-            Unsubscribe(args.OldItems);
-
-            Subscribe(args.NewItems);
-
-            AggregateState();
-        }
-
-        private void Subscribe(IList items)
-        {
-            if (items == null) return;
-
-            foreach (Item item in items)
-            {
-                item.PropertyChanged += Item_PropertyChanged;
-            }
-        }
-
-        private void Unsubscribe(IList items)
-        {
-            if (items == null) return;
-
-            foreach (Item item in items)
-            {
-                item.PropertyChanged -= Item_PropertyChanged;
-            }
-        }
-
-        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (_aggregateState && e.PropertyName.Equals(nameof(State)))
-                AggregateState();
-        }
-
-        private void AggregateState()
-        {
-            State = Items != null && Items.Any() ?
-                         Items.Aggregate(ByPriority).State :
-                             State.None;
-
-            Count = (State == State.None || State == State.Disabled || State == State.Ok) ? 0 : CountItems(Items, State);
-        }
-
-        private static int CountItems(IEnumerable<Item> items, State state)
-        {
-            int count = 0;
-
-            foreach (var item in items)
-            {
-                if (item.Items != null && item.Items.Any())
-                {
-                    count += CountItems(item.Items, state);
-                }
-                else if (item.State == state)
-                {
-                    count++;
-                }
-            }
-
-            return count;
-        }
-
-        private static Item ByPriority(Item a, Item b)
-        {
-            return a.State.Metadata.Priority > b.State.Metadata.Priority ? a : b;
-        }
     }
 }
--- src/AnyStatus.API/Plugins/Item.cs
+++ src/AnyStatus.API/Plugins/Item.cs
@@ -1,6 +1,9 @@
 ﻿using PubSub;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
@@ -21,6 +24,9 @@ namespace AnyStatus.API
     {
         #region Fields
 
+        private readonly bool _aggregateState;
+
+        private int _count;
         private string _name;
         private int _interval;
         private bool _isExpanded;
@@ -41,7 +47,16 @@ namespace AnyStatus.API
 
         #region Ctor
 
-        public Item()
+        public Item(bool aggregateState)
+        {
+            _aggregateState = aggregateState;
+
+            Items = new ObservableCollection<Item>();
+
+            Items.CollectionChanged += OnCollectionChanged;
+        }
+
+        public Item() : this(false)
         {
             ShowNotifications = true;
             IsEnabled = true;
@@ -53,6 +68,21 @@ namespace AnyStatus.API
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// The number of child items with same state
+        /// </summary>
+        [XmlIgnore]
+        [Browsable(false)]
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                _count = value;
+                OnPropertyChanged();
+            }
+        }
 
         [Browsable(false)]
         public Guid Id { get; set; }
@@ -290,6 +320,91 @@ namespace AnyStatus.API
                         clone.Add((Item)item.Clone());
 
             return clone;
+        }
+
+        #endregion
+
+        #region Folder
+
+
+        public void Clear()
+        {
+            Items?.Clear();
+        }
+
+        public bool Contains(Item item)
+        {
+            return Items != null && Items.Contains(item);
+        }
+
+        private void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            if (!_aggregateState) return;
+
+            Unsubscribe(args.OldItems);
+
+            Subscribe(args.NewItems);
+
+            AggregateState();
+        }
+
+        private void Subscribe(IList items)
+        {
+            if (items == null) return;
+
+            foreach (Item item in items)
+            {
+                item.PropertyChanged += Item_PropertyChanged;
+            }
+        }
+
+        private void Unsubscribe(IList items)
+        {
+            if (items == null) return;
+
+            foreach (Item item in items)
+            {
+                item.PropertyChanged -= Item_PropertyChanged;
+            }
+        }
+
+        private void Item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (_aggregateState && e.PropertyName.Equals(nameof(State)))
+                AggregateState();
+        }
+
+        private void AggregateState()
+        {
+            State = Items != null && Items.Any() ?
+                         Items.Aggregate(ByPriority).State :
+                             State.None;
+
+            Count = (State == State.None || State == State.Disabled || State == State.Ok) ? 0 : CountItems(Items, State);
+        }
+
+        private static int CountItems(IEnumerable<Item> items, State state)
+        {
+            int count = 0;
+
+            foreach (var item in items)
+            {
+                if (item.Items != null && item.Items.Any())
+                {
+                    count += CountItems(item.Items, state);
+                }
+                else if (item.State == state)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static Item ByPriority(Item a, Item b)
+        {
+            return a.State.Metadata.Priority > b.State.Metadata.Priority ? a : b;
         }
 
         #endregion
