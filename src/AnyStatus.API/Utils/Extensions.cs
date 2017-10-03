2756ff9e9c7515a0d89d6c8951cdfecc5abb88df
A763847 <alon.amsalem@gmail.com>
2017-10-04 02:02:24 +0300

    set attr field value ext

---
+++ src/AnyStatus.API/Utils/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace AnyStatus.API.Utils
+{
+    public static class Extensions
+    {
+        public static void SetAttributeFieldValue<T>(this object obj, string propertyName, string fieldName, object value) where T : Attribute
+        {
+            if (string.IsNullOrEmpty(propertyName))
+                throw new ArgumentNullException(nameof(propertyName));
+
+            if (string.IsNullOrEmpty(fieldName))
+                throw new ArgumentNullException(nameof(fieldName));
+
+            var desc = TypeDescriptor.GetProperties(obj.GetType())[propertyName];
+            var attr = (T)desc.Attributes[typeof(T)];
+            var field = attr.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            field?.SetValue(attr, value);
+        }
+    }
+}
