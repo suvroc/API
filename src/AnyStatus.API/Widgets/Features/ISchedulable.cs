82ac12782dccc7a71ce732db4c6176ce2e056fbd
AlonAm <alon.amsalem@gmail.com>
2018-02-28 19:55:11 +0200

    schedulable props

--- src/AnyStatus.API/Widgets/Features/ISchedulable.cs
+++ src/AnyStatus.API/Widgets/Features/ISchedulable.cs
@@ -1,6 +1,13 @@
-﻿namespace AnyStatus.API
+﻿using System;
+
+namespace AnyStatus.API
 {
     public interface ISchedulable
     {
+        Guid Id { get; }
+
+        string Name { get; }
+
+        int Interval { get; }
     }
 }
