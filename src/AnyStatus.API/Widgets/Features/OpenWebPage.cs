e692afe89b6bdd347da9a009bd08be993990f3f4
AlonAm <alon.amsalem@gmail.com>
2018-03-08 12:09:06 +0200

    default web page opener

--- src/AnyStatus.API/Widgets/Features/OpenWebPage.cs
+++ src/AnyStatus.API/Widgets/Features/OpenWebPage.cs
@@ -1,4 +1,6 @@
-﻿namespace AnyStatus.API
+﻿using System;
+
+namespace AnyStatus.API
 {
     public interface IWebPage
     {
@@ -19,6 +21,28 @@
         public static OpenWebPageRequest<T> Create<T>(T context) where T : IWebPage
         {
             return new OpenWebPageRequest<T>(context);
+        }
+    }
+
+    public class OpenWebPage<TWebPage> : RequestHandler<OpenWebPageRequest<TWebPage>>, IOpenWebPage<TWebPage> where TWebPage : IWebPage
+    {
+        private readonly IProcessStarter _ps;
+
+        public OpenWebPage(IProcessStarter ps)
+        {
+            _ps = ps ?? throw new ArgumentNullException();
+        }
+
+        protected override void HandleCore(OpenWebPageRequest<TWebPage> request)
+        {
+            var uri = request.DataContext.URL;
+
+            if (!Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+            {
+                throw new UriFormatException(uri);
+            }
+
+            _ps.Start(uri);
         }
     }
 }
