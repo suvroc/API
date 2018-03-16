98337db7abb7003ff120cf9f901e4c4db4b0a672
AlonAm <alon.amsalem@gmail.com>
2018-03-16 02:34:47 +0200

    extended logger

--- src/AnyStatus.API/Common/Services/ILogger.cs
+++ src/AnyStatus.API/Common/Services/ILogger.cs
@@ -8,6 +8,12 @@ namespace AnyStatus.API
 
         void Info(string message);
 
+        void Debug(string message);
+
+        void Trace(string message);
+
+        void Error(string message);
+
         void Error(Exception exception);
 
         void Error(Exception exception, string message);
