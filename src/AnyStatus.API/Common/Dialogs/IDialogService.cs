d530b61e9aeb6e0a876e9f29d4bedd28d1545148
Alon Amsalem <alon@anystat.us>
2018-12-04 22:56:22 +0200

    single show-dialog method

--- src/AnyStatus.API/Common/Dialogs/IDialogService.cs
+++ src/AnyStatus.API/Common/Dialogs/IDialogService.cs
@@ -3,17 +3,5 @@
     public interface IDialogService
     {
         DialogResult ShowDialog(IDialog dialog);
-
-        DialogResult ShowDialog(ConfirmationDialog dialog);
-
-        DialogResult ShowDialog(ErrorDialog dialog);
-
-        DialogResult ShowDialog(InfoDialog dialog);
-
-        DialogResult ShowDialog(OpenFileDialog dialog);
-
-        DialogResult ShowDialog(SaveFileDialog dialog);
-
-        DialogResult ShowDialog(WarningDialog dialog);
     }
 }
